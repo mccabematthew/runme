da5278087d6223bb0ae5d172e412cf84943dc9e0
Matthew McCabe <matthew.james.mccabe@outlook.com>
2026-03-11 21:43:41 -0500

    add login, signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Dashboard from './pages/Dashboard'
 import LogRun from './pages/LogRun'
+import SignupPage from './pages/SignUp'
+import LoginPage from './pages/Login'
+
+function AppLayout({ children }) {
+  return (
+    <div className="min-h-screen bg-background text-foreground w-full max-w-4xl mx-auto px-4 pb-16">
+      {children}
+      <NavBar />
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-background text-foreground w-full max-w-4xl mx-auto px-4 pb-16">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/log" element={<LogRun />} />
-          <Route path="/history" element={<div className="p-4">History</div>} />
-          <Route path="/login" element={<div className="p-4">Login</div>} />
-        </Routes>
-        <NavBar />
-      </div>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<AppLayout><Dashboard /></AppLayout>} />
+        <Route path="/log" element={<AppLayout><LogRun /></AppLayout>} />
+        <Route path="/history" element={<AppLayout><div className="p-4">History</div></AppLayout>} />
+      </Routes>
     </BrowserRouter>
   )
 }
